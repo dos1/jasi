948b81cc94a3ea5738d2fd36cb68afd44458fc1a
Sebastian Krzyszkowiak <dos@dosowisko.net>
2013-12-12 01:53:19 +0100

    scaffolded angular app with semantic ui and friends

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,10 @@
+'use strict';
+
+angular.module('jasiApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
